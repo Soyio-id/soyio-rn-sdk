208f36fc74ba453f383f892d94e74714bd8d8887
Ignacio Mendez <ignacio.mendez.perez@gmail.com>
2024-03-28 10:01:02 -0300

    fix(Android): fix redirect

--- src/core.tsx
+++ src/core.tsx
@@ -2,10 +2,10 @@ import { useCallback } from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import { getFlowUrl, buildUrlParams } from './utils';
 import { SOYIO_REDIRECT_URL } from './constants';
-
+import { Platform } from 'react-native';
 import type { SoyioWidgetViewPropsType, RegisterParams, AuthenticateParams } from './types';
 
-export const useSoyioAuth = ({ options, onEventChange }: SoyioWidgetViewPropsType) => {
+async function getBrowserOptions() {
   const webBrowserOptions: WebBrowser.AuthSessionOpenOptions = {
     dismissButtonStyle: 'cancel',
     createTask: false,
@@ -13,11 +13,22 @@ export const useSoyioAuth = ({ options, onEventChange }: SoyioWidgetViewPropsTyp
     showTitle: true,
   };
 
+  if (Platform.OS === "android") {
+    const { preferredBrowserPackage } = await WebBrowser.getCustomTabsSupportingBrowsersAsync();
+    webBrowserOptions.browserPackage = preferredBrowserPackage;
+  }
+
+  return webBrowserOptions;
+}
+
+export const useSoyioAuth = ({ options, onEventChange }: SoyioWidgetViewPropsType) => {
+
   const register = useCallback(async (registerParams: RegisterParams) => {
     const registerBaseUri = getFlowUrl(options, 'register');
     const registerUri = `${registerBaseUri}?${buildUrlParams(options, registerParams)}`;
 
     if (onEventChange) onEventChange({ type: 'open register' });
+    const webBrowserOptions = await getBrowserOptions();
 
     const registerResult = await WebBrowser.openAuthSessionAsync(
       registerUri,
@@ -33,6 +44,8 @@ export const useSoyioAuth = ({ options, onEventChange }: SoyioWidgetViewPropsTyp
     const authenticateUri = `${authenticateBaseUri}?${buildUrlParams(options, authenticateParams)}`;
 
     if (onEventChange) onEventChange({ type: 'open authenticate' });
+    const webBrowserOptions = await getBrowserOptions();
+
     const authenticateResult = await WebBrowser.openAuthSessionAsync(
       authenticateUri,
       SOYIO_REDIRECT_URL,
@@ -43,3 +56,12 @@ export const useSoyioAuth = ({ options, onEventChange }: SoyioWidgetViewPropsTyp
 
   return { register, authenticate };
 };
+
+async function openInCustomTab(url) {
+  if (Platform.OS === "android") {
+      const {preferredBrowserPackage} = await WebBrowser.getCustomTabsSupportingBrowsersAsync()
+      await WebBrowser.openBrowserAsync(url, {browserPackage: preferredBrowserPackage})
+  } else {
+      await WebBrowser.openBrowserAsync(url)
+  }
+}
