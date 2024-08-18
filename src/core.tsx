ccfb4436b521419d6ce742bd0989c49aa17dc996
ignaciomendezp <ignacio.mendez.perez@gmail.com>
2024-08-18 16:21:14 -0400

    feat(DataAccess): add data access and refactor event output

--- src/core.tsx
+++ src/core.tsx
@@ -2,77 +2,47 @@ import * as WebBrowser from 'expo-web-browser';
 import { useCallback } from 'react';
 
 import type {
-  AuthenticateParams,
-  RegisterParams,
-  SignatureParams,
+  DisclosureParams,
   SoyioWidgetViewPropsType,
 } from './types';
 import {
   buildUrlParams,
-  ERROR_URL_REGEX,
   getBrowserOptions,
   getFlowUrl,
   getRedirectUrl,
+  parseUrlResponseParams,
 } from './utils';
 
 export const useSoyioAuth = ({ options, onEventChange }: SoyioWidgetViewPropsType) => {
-  const register = useCallback(async (registerParams: RegisterParams) => {
-    const registerBaseUri = getFlowUrl(options, 'register');
-    const registerUri = `${registerBaseUri}?${buildUrlParams(options, registerParams)}`;
+  const disclosure = useCallback(async (registerParams: DisclosureParams) => {
+    const disclosureBaseUri = getRequestUrl(options, 'disclosure');
+    const disclosureUri = `${disclosureBaseUri}?${buildUrlParams(options, registerParams)}`;
     const redirectUrl = getRedirectUrl(options.uriScheme);
     const webBrowserOptions = await getBrowserOptions();
 
-    if (onEventChange) onEventChange({ type: 'open register' });
+    if (onEventChange) onEventChange({ type: 'open_disclosure' });
 
-    const registerResult = await WebBrowser.openAuthSessionAsync(
-      registerUri,
+    const disclosureResult = await WebBrowser.openAuthSessionAsync(
+      disclosureUri,
       redirectUrl,
       webBrowserOptions,
     );
 
-    if ((registerResult.type === 'success') && (registerResult.url?.includes('error'))) {
-      const errorMessage = registerResult.url.match(ERROR_URL_REGEX)[1];
-
-      if (onEventChange) onEventChange({ type: 'error', message: errorMessage });
-    } else if (onEventChange) onEventChange(registerResult);
+    if (onEventChange) {
+      // 'success' type means that a redirection was triggered by Soyio,
+      // but doesn't mean that the process was successful
+      if (disclosureResult.type === 'success') {
+        const urlParams = parseUrlResponseParams(disclosureResult.url);
+        if (disclosureResult.url?.includes('error')) {
+          onEventChange({ type: 'error', ...urlParams });
+        } else {
+          onEventChange({ type: 'success', ...urlParams });
+        }
+      } else {
+        onEventChange(disclosureResult);
+      }
+    }
   }, [options, onEventChange]);
 
-  const authenticate = useCallback(async (authenticateParams: AuthenticateParams) => {
-    const authenticateBaseUri = getFlowUrl(options, 'authenticate');
-    const authenticateUri = `${authenticateBaseUri}?${buildUrlParams(options, authenticateParams)}`;
-    const redirectUrl = getRedirectUrl(options.uriScheme);
-    const webBrowserOptions = await getBrowserOptions();
-
-    if (onEventChange) onEventChange({ type: 'open authenticate' });
-
-    const authenticateResult = await WebBrowser.openAuthSessionAsync(
-      authenticateUri,
-      redirectUrl,
-      webBrowserOptions,
-    );
-    if (onEventChange) onEventChange(authenticateResult);
-  }, [options, onEventChange]);
-
-  const signature = useCallback(async (signatureParams: SignatureParams) => {
-    const signatureBaseUri = getFlowUrl(options, 'signature');
-    const signatureUri = `${signatureBaseUri}?${buildUrlParams(options, signatureParams)}`;
-    const redirectUrl = getRedirectUrl(options.uriScheme);
-    const webBrowserOptions = await getBrowserOptions();
-
-    if (onEventChange) onEventChange({ type: 'open signature' });
-
-    const signatureResult = await WebBrowser.openAuthSessionAsync(
-      signatureUri,
-      redirectUrl,
-      webBrowserOptions,
-    );
-
-    if ((signatureResult.type === 'success') && (signatureResult.url?.includes('error'))) {
-      const errorMessage = signatureResult.url.match(ERROR_URL_REGEX)[1];
-
-      if (onEventChange) onEventChange({ type: 'error', message: errorMessage });
-    } else if (onEventChange) onEventChange(signatureResult);
-  }, [options, onEventChange]);
-
-  return { register, authenticate, signature };
+  return { disclosure };
 };
