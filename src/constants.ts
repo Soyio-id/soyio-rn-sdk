06709e49c8d319f03fa5f687501cc4de5b923e46
Ignacio Mendez Perez <ignacio.mendez.perez@gmail.com>
2025-12-15 17:24:27 -0300

    chore(release): add NODE_AUTH_TOKEN to npm publish step

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 export const PRODUCTION_URL = 'https://app.soyio.id';
 export const SANDBOX_URL = 'https://sandbox.soyio.id';
 
-// URLs for the Soyio API
 export const SOYIO_BASE_URLS = [PRODUCTION_URL, SANDBOX_URL];
